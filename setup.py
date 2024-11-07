cb125acd86a65f152bb68b96b9321c90aada5413
Lukaaaa13 <as.daum.uni@gmail.com>
2024-11-07 22:31:30 +0100

    tried another idea for setup with entry point to console

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-from distutils.core import setup
-setup(name='math_quiz',
-      version='1.0',
-      py_modules=['math_quiz'],
-      )
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='1.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'math_quiz=math_quiz.math_quiz:math_quiz',
+        ],
+    },
+)
